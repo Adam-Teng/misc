713193d753a2feacb44d0aaebbc173df3a74a5bb
Adam-Teng <656081024@qq.com>
2022-07-21 16:59:39 +0800

    recursion binary search

--- THUOJ/range/brute_range.c
+++ THUOJ/range/brute_range.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int cmp(const void *a, const void *b) { return *((int *)a) - *((int *)b); }
+
 int binary_search_a(int lst[], int a, int x, int y) {
   if (a <= lst[x]) {
     return x;
@@ -9,7 +11,7 @@ int binary_search_a(int lst[], int a, int x, int y) {
   } else if (a == lst[y]) {
     return y;
   } else if (a <= lst[(x + y) / 2 + 1] && a > lst[(x + y) / 2]) {
-    return (x + y) / 2 + 1;
+    return (x + y) / 2;
   } else if (a <= lst[(x + y) / 2]) {
     return binary_search_a(lst, a, x, (x + y) / 2);
   } else {
@@ -20,10 +22,10 @@ int binary_search_a(int lst[], int a, int x, int y) {
 int binary_search_b(int lst[], int a, int x, int y) {
   if (a < lst[x]) {
     return x;
-  } else if (a == lst[y]) {
-    return y + 1;
+  } else if (a == lst[x]) {
+    return x + 1;
   } else if (a >= lst[y]) {
-    return y;
+    return y + 1;
   } else if (a < lst[(x + y) / 2 + 1] && a >= lst[(x + y) / 2]) {
     return (x + y) / 2 + 1;
   } else if (a < lst[(x + y) / 2]) {
@@ -39,9 +41,19 @@ int range(int lst[], int a, int b, int len) {
 }
 
 int main() {
-  int lst[5] = {1, 3, 7, 9, 11};
-  int len = 5;
-  int a = 0, b = 11;
-  printf("%d\n", range(lst, a, b, len));
+  int m, n;
+  scanf("%d %d", &m, &n);
+  int lst[m];
+  int i = 0;
+  for (i = 0; i < m; i++) {
+    scanf("%d", &lst[i]);
+  }
+  qsort(lst, m, sizeof(int), cmp);
+  int a, b, x;
+  for (i = 0; i < n; i++) {
+    scanf("%d %d", &a, &b);
+    x = range(lst, a, b, m);
+    printf("%d\n", x);
+  }
   return 0;
 }
